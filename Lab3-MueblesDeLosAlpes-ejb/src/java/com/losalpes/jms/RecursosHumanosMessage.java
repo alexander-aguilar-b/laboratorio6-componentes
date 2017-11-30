c8fb523f7e47048d8c5b9122bd7e04e66127e735
Edgar Aguilar <edgaguil@publicisgroupe.net>
2017-11-30 14:47:03 -0500

    Actividades preliminares

---
+++ Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/jms/RecursosHumanosMessage.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.losalpes.jms;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ *
+ * @author edgaguil
+ */
+@MessageDriven(mappedName = "jms/cambioDeCargoTopic", activationConfig = {
+    @ActivationConfigProperty(propertyName = "clientId", propertyValue = "RecursosHumanosMessage"),
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/cambioDeCargoTopic"),
+    @ActivationConfigProperty(propertyName = "subscriptionDurability", propertyValue = "Durable"),
+    @ActivationConfigProperty(propertyName = "subscriptionName", propertyValue = "jms/cambioDeCargoTopic"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic")
+})
+public class RecursosHumanosMessage implements MessageListener {
+
+    @Resource
+    private MessageDrivenContext mdc;
+
+    public RecursosHumanosMessage() {
+    }
+
+    @Override
+    public void onMessage(Message message) {
+        TextMessage msg = null;
+        try {
+            if (message instanceof TextMessage) {
+                msg = (TextMessage) message;
+                Logger.getLogger(RecursosHumanosMessage.class.getName()).log(Level.INFO,
+                        "Recursos Humanos: Se ha recibido la notificación de modificación del empleado \n" + msg.getText());
+            } else {
+                Logger.getLogger(RecursosHumanosMessage.class.getName()).log(Level.SEVERE,
+                        "Mensaje de tipo equivocado: " + message.getClass().getName());
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+            mdc.setRollbackOnly();
+        } catch (Throwable te) {
+            te.printStackTrace();
+        }
+    }
+
+}
